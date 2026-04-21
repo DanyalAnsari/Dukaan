d8dfa1ac662ca81e99184573600ad0e71696b5bc
DanyalAnsari <mh.danyalansari@gmail.com>
2026-04-21 13:16:56 +0530

    refactor: minor code refactor

--- lib/get-session.ts
+++ lib/get-session.ts
@@ -4,8 +4,9 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { cache } from "react";
 
-export const getSession = cache(async () => {
-  return await auth.api.getSession({
-    headers: await headers(),
-  });
-});
+export const getSession = cache(
+  async () =>
+    await auth.api.getSession({
+      headers: await headers(),
+    })
+);
